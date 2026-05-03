c114138450bf56c2c1759c15edf3af3b3f476a84
GhostyTongue <191202026+Gh0styTongue@users.noreply.github.com>
2026-05-02 17:27:05 -0700

    Delete src directory

--- src/lib/toTitleCase.ts
+++
@@ -1,6 +0,0 @@
-export const toTitleCase = (str: string) => {
-  return str
-    .split(' ')
-    .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
-};
--- src/shared/text-tracks.tsx
+++
@@ -1,7 +0,0 @@
-import { VideoJsPlayer } from 'video.js';
-
-export const getTextTracks = (player: VideoJsPlayer): TextTrackList => {
-  return player && player.remoteTextTracks().length > 0
-    ? player.remoteTextTracks()
-    : player.textTracks();
-};
